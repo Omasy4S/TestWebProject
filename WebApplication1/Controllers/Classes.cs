4d7ed7a6d8575a620c19845a1cec6a0b3e6861f0
Omyy <poshlivy@yandex.ru>
2025-08-31 15:56:55 +0300

    Были добавлены выводы в представления

---
+++ WebApplication1/Controllers/Classes.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Controllers
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class CreateUsersViewModel
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        [MaxLength(50, ErrorMessage = "Длина не должна превышать 50 символов")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите возраст")]
+        [Range(1,110,ErrorMessage = "Недопустимый возраст")]
+        public int Age { get; set; }
+
+    }
+    public class CalculateTwoNumbers
+    {
+        [Required(ErrorMessage = "Введите число")]
+        [Range(1,1000000,ErrorMessage = "Первое число слишком большое")]
+        public int FirstValue { get; set; }
+        [Required(ErrorMessage = "Введите число")]
+        [Range(1, 1000000, ErrorMessage = "Второе число слишком большое")]
+        public int SecondValue { get; set; }
+        public int Result { get; set; }
+    }
+}
--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -11,11 +11,59 @@ namespace WebApplication1.Controllers
         {
         }
 
+        public IActionResult PrintValue()
+        {
+            //вывод одного значения
+            int testValue = 16;
+            return View(testValue);
+        }
+        public IActionResult PrintValueCollection()
+        {
+            //вывод нескольких значений
+            var users = new List<User>
+            {
+                new User {Name = "Максим", Age = 23},
+                new User {Name = "Олег", Age = 25},
+                new User {Name = "Александр", Age = 15},
+                new User {Name = "Вячеслав", Age = 21},
+                new User {Name = "Ирина", Age = 30},
+                new User {Name = "Екатерина", Age = 18},
+            };
+            return View(users);
+        }
+
+        //получение значений
+        [HttpGet]
+        public IActionResult Calculation() => View();
+
+        //выполнение после получения значений
+        [HttpPost]
+        public IActionResult Calculation(CalculateTwoNumbers model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Result = model.FirstValue + model.SecondValue;
+            }
+            return View(model);
+        }
+
+        [HttpGet] 
+        public IActionResult CreateUser() => View();
+
+        [HttpPost]
+        public IActionResult CreateUser(CreateUsersViewModel model)
+        {
+            //ничего не делает, но получает значения для дальнейших дествий, по типу запись в бд
+            return View(model);
+        }
         public IActionResult Index()
         {
             return View();
         }
-
+        public IActionResult Output()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
