adaf008f068023b772c30272ece9ca93bf8b7920
Omyy <poshlivy@yandex.ru>
2025-09-02 16:22:14 +0300

    Добавлена страница политики, внесены правки в код

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,91 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Models;
+using TestWebProject.Models;
 
-namespace WebApplication1.Controllers
+namespace TestWebProject.Controllers
 {
     public class HomeController : Controller
     {
-
-        public HomeController()
-        {
-        }
-
-        public IActionResult PrintValue()
-        {
-            //вывод одного значения
-            int testValue = 16;
-            return View(testValue);
-        }
-        public IActionResult PrintValueCollection()
-        {
-            //вывод нескольких значений
-            var users = new List<User>
-            {
-                new User {Name = "Максим", Age = 23},
-                new User {Name = "Олег", Age = 25},
-                new User {Name = "Александр", Age = 15},
-                new User {Name = "Вячеслав", Age = 21},
-                new User {Name = "Ирина", Age = 30},
-                new User {Name = "Екатерина", Age = 18},
-            };
-            return View(users);
-        }
-
-        //получение значений
-        [HttpGet]
-        public IActionResult Calculation() => View();
-
-        //выполнение после получения значений
-        [HttpPost]
-        public IActionResult Calculation(CalculateTwoNumbers model)
-        {
-            if (ModelState.IsValid)
-            {
-                model.Result = model.FirstValue + model.SecondValue;
-            }
-            return View(model);
-        }
-
-        [HttpGet] 
-        public IActionResult CreateUser() => View();
-
-        [HttpPost]
-        public IActionResult CreateUser(CreateUsersViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-
-                // Устанавливаем флаг, что пользователь был добавлен
-                ViewBag.UserAdded = true;
-
-                // Можно также установить сообщение для TempData
-                TempData["SuccessMessage"] = "Пользователь успешно создан!";
-
-                // Очищаем модель для формы
-                ModelState.Clear();
-
-                // Возвращаем представление с пустой моделью
-                return View(new CreateUsersViewModel());
-            }
-
-            // Если модель невалидна, возвращаем её с ошибками
-            return View(model);
-        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Output()
-        {
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        public IActionResult PrintInfo()
+        public IActionResult Privacy()
         {
             return View();
         }
--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -1,8 +1,8 @@
-namespace WebApplication1.Models
+namespace TestWebProject.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string? RequestId { get; init; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
